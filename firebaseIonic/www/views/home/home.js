cb3bc80f72b28973bb65f9613ea17506a2ff6255
CristianP <corpari25@gmail.com>
2016-07-30 10:46:01 -0500

    Adición Código QR
    Se ha adicionado un botón en la pantalla home para comenzar el escaneo
    del código QR con la cámara del dispositivo, al leer el código se
    muestra en un alert el mensaje del código.

--- firebaseIonic/www/views/home/home.js
+++ firebaseIonic/www/views/home/home.js
@@ -1,5 +1,5 @@
 'Use Strict';
-angular.module('App').controller('homeController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
+angular.module('App').controller('homeController', function ($scope, $cordovaBarcodeScanner, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
   var ref = firebase.database().ref();
 
   $scope.logOut = function () {
@@ -10,6 +10,14 @@ angular.module('App').controller('homeController', function ($scope, $state,$cor
   $scope.profile = function () {
       $location.path("/profile");
   }
+
+  $scope.leerCodigo = function() {
+            $cordovaBarcodeScanner.scan().then (function (imagenEscaneada) {
+                alert(imagenEscaneada.text);
+            },function(error) {
+                alert("ha ocurrido un error : "+error);
+            });
+        }
 
 }
 );
